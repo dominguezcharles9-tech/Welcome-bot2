813961c4382d52f52f7498b83adb22de94fc15d4
dominguezcharles9-tech <dominguezcharles9@gmail.com>
2026-01-16 20:40:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    token : "mongodb+srv://Baby:Ctanginamo1@cluster0.hsiuqjh.mongodb.net/?appName=Cluster0",
-    mongoURI : "MONGO_URI",
+    token : "",
+    mongoURI : "mongodb+srv://Baby:Ctanginamo1@cluster0.hsiuqjh.mongodb.net/?appName=Cluster0",
     statusType: 3, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
     statusContent: "A", // A = Show total users count, B = Show total servers count, C = Custom message
     customStatus: "xinloverboy",
     streamingURL: ""
 
 };
+
