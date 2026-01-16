3217c717b29c96d56a9adf65c9892fac0ae6be43
dominguezcharles9-tech <dominguezcharles9@gmail.com>
2026-01-16 20:32:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    token : "",
-    mongoURI : "",
+    token : "mongodb+srv://Baby:Ctanginamo1@cluster0.hsiuqjh.mongodb.net/?appName=Cluster0",
+    mongoURI : "MONGO_URI",
     statusType: 3, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
     statusContent: "A", // A = Show total users count, B = Show total servers count, C = Custom message
-    customStatus: "GlaceYT",
-    streamingURL: "https://www.twitch.tv/glaceytt"
+    customStatus: "xinloverboy",
+    streamingURL: ""
+
 };
